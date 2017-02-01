e064c65dad9f029ec887edb04f82be9ec9921e7e
Sergei Bobrov <pandaphotoshopdesigner@gmail.com>
2017-02-01 15:50:10 +0800

    changed parameters for component

--- local/components/evrysoft/external.user.report/.parameters.php
+++ local/components/evrysoft/external.user.report/.parameters.php
@@ -4,33 +4,34 @@ if(!defined("B_PROLOG_INCLUDED")||B_PROLOG_INCLUDED!==true)die();
 $arComponentParameters = array(
 	'PARAMETERS' => array(
 		'DATE_BEFORE' => [
-			'NAME' => 'DATE_BEFORE',
+			'NAME' => 'Вывод до',
 			'TYPE' => 'DATE'
 		],
 		'DATE_AFTER' => [
-			'NAME' => 'DATE_AFTER',
+			'NAME' => 'Вывод после',
 			'TYPE' => 'DATE'
 		],
 		'DEBUG' => [
-			'NAME' => 'DEBUG',
+			'NAME' => 'Режим отладки',
 			'TYPE' => 'CHECKBOX'
 		],
 		'LIMIT' => [
-			'NAME' => 'LIMIT',
+			'NAME' => 'Количество записей на странице',
 			'TYPE' => 'STRING'
 		],
 		'USE_PRELOAD' => [
-			'NAME' => 'USE_PRELOAD',
+			'NAME' => 'Использовать preloader',
 			'TYPE' => 'CHECKBOX'
 		],
 		'VIEW_TYPE' => [
-			'NAME' => 'VIEW_TYPE',
+			'NAME' => 'Режим вывода',
 			'TYPE' => 'LIST',
 			'VALUES' => [
 				'FILE' => 'LIKE A FILE',
 				'TABLE' => 'LIKE A TABLE',
 				'BLOCK' => 'LIKE A BLOCK'
-			]
+			],
+			'PARENT' => 'BASE'
 		]
 	),
 );
