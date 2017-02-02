1622de68f34249cf9ae032a51d4a6205e3e86044
Alexander Karpov <slayervc@mail.ru>
2017-02-02 15:20:01 +0800

    import of partners and catefories added

--- test/clone_partners.php
+++ test/clone_partners.php
@@ -2,7 +2,23 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Клонировать партнеров");
 
+require($_SERVER["DOCUMENT_ROOT"]."/test/partner_import.php");
 
+$partnerList = getPartnerList(1);
+//$categoryList = getCategoryList();
 
+//var_dump($partnerList, $categoryList);
+
+foreach ($partnerList as $partner) {
+    $result = addPartnerToInfoblock($partner);
+    echo $result ? $result : 'Проблемы при подключении модуля инфоблоков.';
+    echo '<BR>';
+}
+
+/*foreach ($categoryList as $category) {
+    $result = addCategoryToInfoblock($category);
+    echo $result ? $result : 'Проблемы при подключении модуля инфоблоков.';
+    echo '<BR>';
+}*/
 
 ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
