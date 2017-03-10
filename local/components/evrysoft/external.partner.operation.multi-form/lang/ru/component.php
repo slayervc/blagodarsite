7a30b5a9d14cfc8248cdb0544db95dad389cd326
Sergei Bobrov <pandaphotoshopdesigner@gmail.com>
2017-03-10 14:29:57 +0800

    updated form inputs localisation files

--- local/components/evrysoft/external.partner.operation.multi-form/lang/ru/component.php
+++ local/components/evrysoft/external.partner.operation.multi-form/lang/ru/component.php
@@ -3,7 +3,7 @@
 $MESS['SUBMIT_BUTTON'] = 'Отправить';
 $MESS['sum'] = 'Сумма бонусов';
 $MESS['login'] = 'Номер клиента';
-$MESS['login_or_ean13'] = 'Логин клиента или ean13';
+$MESS['login_or_ean13'] = 'Номер телефона';
 $MESS['partner_id'] = 'Индентификатор партнёра';
 $MESS['code'] = 'Код из СМС';
 $MESS['name'] = 'ФИО клиента';
@@ -21,4 +21,24 @@ $MESS['gen-balance-code'] = 'Отправить клиенту SMS с кодом
 $MESS['remove-client-balance'] = 'Списание с баланс клиента';
 $MESS['report'] = 'partners/getreport';
 
-
+// SUBMITS
+// 
+$MESS['SUBMIT_info'] = 'Информация о партнере по его номеру';
+$MESS['SUBMIT_partner-list'] = 'Получить список партнеров';
+$MESS['SUBMIT_add-partner-balance'] = 'Пополнение баланса другого партнёра';
+$MESS['SUBMIT_gen-reg-code'] = 'Отправить клиенту SMS код для регистрации';
+$MESS['SUBMIT_regclient'] = 'Добавить';
+$MESS['SUBMIT_get-client-info'] = 'Получить';
+$MESS['SUBMIT_add-client-balance'] = 'Пополнить';
+$MESS['SUBMIT_add-client-balance-proc'] = 'Пополнить';
+$MESS['SUBMIT_gen-balance-code'] = 'Отправить клиенту SMS с кодом для списания';
+$MESS['SUBMIT_remove-client-balance'] = 'Списать';
+$MESS['SUBMIT_add-category'] = 'Добавить категорию';
+$MESS['SUBMIT_set-category'] = 'Изменить категорию по ее ID';
+$MESS['SUBMIT_get-categories'] = 'Получить список категорий';
+$MESS['SUBMIT_get-category-info'] = 'Информация категории по ее ID';
+$MESS['SUBMIT_get-cities-list'] = 'Получить список городов';
+$MESS['SUBMIT_add-partner-by-agent'] = 'Добавить партнера';
+$MESS['SUBMIT_set-partner-by-agent'] = 'Добавить партнера';
+$MESS['SUBMIT_get-partners-list-agent'] = 'Получить список партнеров';
+$MESS['SUBMIT_get-info-partner-agent'] = 'Информация по партнеру по ID';
